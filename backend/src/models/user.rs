1624931e68922bcc711271d21f8bdd251687d56b
noamfav <noam.favier@icloud.com>
2024-10-22 01:15:36 +0200

    dn

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -5,4 +5,9 @@ use serde::{Deserialize, Serialize};
 pub struct User {
     pub id: i32,
     pub username: String,
+    pub email: String,
+    pub password: String,
+    pub created_at: String,
+    pub updated_at: String,
+    pub is_admin: bool,
 }
