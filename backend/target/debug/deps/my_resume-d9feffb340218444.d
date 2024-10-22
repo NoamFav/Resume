81b69fad819e52de2098b2295d3471c42201c842
noamfav <noam.favier@icloud.com>
2024-10-22 16:26:21 +0200

    i don't really know at this point

--- backend/target/debug/deps/my_resume-d9feffb340218444.d
+++ backend/target/debug/deps/my_resume-d9feffb340218444.d
@@ -1,12 +1,13 @@
-/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/libmy_resume-d9feffb340218444.rmeta: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
+/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/libmy_resume-d9feffb340218444.rmeta: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/models/programming_languages.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
 
-/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/my_resume-d9feffb340218444.d: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
+/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/my_resume-d9feffb340218444.d: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/models/programming_languages.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
 
 src/main.rs:
 src/config.rs:
 src/database.rs:
 src/models/mod.rs:
 src/models/user.rs:
+src/models/programming_languages.rs:
 src/routes/mod.rs:
 src/routes/auth_routes.rs:
 src/routes/user_routes.rs:
--- backend/target/debug/deps/my_resume-f1f579e8fc8e1ca0.d
+++ backend/target/debug/deps/my_resume-f1f579e8fc8e1ca0.d
@@ -1,12 +1,13 @@
-/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/libmy_resume-f1f579e8fc8e1ca0.rmeta: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
+/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/libmy_resume-f1f579e8fc8e1ca0.rmeta: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/models/programming_languages.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
 
-/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/my_resume-f1f579e8fc8e1ca0.d: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
+/Users/noamfavier/Git_projects/Resume/backend/target/debug/deps/my_resume-f1f579e8fc8e1ca0.d: src/main.rs src/config.rs src/database.rs src/models/mod.rs src/models/user.rs src/models/programming_languages.rs src/routes/mod.rs src/routes/auth_routes.rs src/routes/user_routes.rs src/services/mod.rs src/services/auth_service.rs src/services/user_service.rs
 
 src/main.rs:
 src/config.rs:
 src/database.rs:
 src/models/mod.rs:
 src/models/user.rs:
+src/models/programming_languages.rs:
 src/routes/mod.rs:
 src/routes/auth_routes.rs:
 src/routes/user_routes.rs:
