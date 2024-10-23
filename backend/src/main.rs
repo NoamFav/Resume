ba20f6a13a378ebebd2cb1a89dcb846d421281a2
noamfav <95364866+NoamFav@users.noreply.github.com>
2024-10-24 01:11:26 +0200

    done db and bd connected

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,36 +1,43 @@
-// backend/src/main.rs
+// src/main.rs
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use log::info;
+use log::{info};
 
-// Module declarations
 mod config;
 mod database;
 mod models;
 mod routes;
 mod services;
+mod schema;
 
-// A simple handler for the root path
+use models::ProgrammingLanguage;
+
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello, world!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Initialize configuration and logging
     config::init();
 
     info!("Starting server at http://0.0.0.0:8000");
 
-    // Create the database pool
-    let pool = database::connect().await;
+    let pool = database::establish_connection();
+    info!("Connected to database");
 
-    // Start the HTTP server
+    let programming_languages = {
+        let mut conn = pool.get().expect("Failed to get a connection from the pool");
+
+        ProgrammingLanguage::all(&mut conn).expect("Failed to load programming languages")
+    };
+
+    info!("Programming Languages: {:?}", programming_languages);
+
     HttpServer::new(move || {
         App::new()
-            .data(pool.clone()) // Make the pool available to handlers
-            .configure(routes::user_routes::config) // Register user routes
-            .configure(routes::auth_routes::config) // Register auth routes
-            .route("/", web::get().to(index)) // Root route
+            .app_data(web::Data::new(pool.clone()))
+            .configure(routes::user_routes::config)
+            .configure(routes::auth_routes::config)
+            .route("/", web::get().to(index))
     })
     .bind(("0.0.0.0", 8000))?
     .run()
--- backend/src/models/programming_languages.rs
+++ backend/src/models/programming_languages.rs
@@ -1,9 +1,20 @@
-// backend/src/models/user.rs
+// backend/src/models/programming_languages.rs
 use serde::{Deserialize, Serialize};
+use diesel::prelude::*;
+use diesel::mysql::MysqlConnection;
+use crate::schema::programming_languages;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Queryable, Serialize, Deserialize, Debug)]
 pub struct ProgrammingLanguage {
     pub id: i32,
     pub name: String,
     pub percentage: f32,
+    pub favorite: Option<bool>,
+    pub learn: Option<bool>,
+}
+
+impl ProgrammingLanguage {
+    pub fn all(conn: &mut MysqlConnection) -> QueryResult<Vec<ProgrammingLanguage>> {
+        programming_languages::table.load::<ProgrammingLanguage>(conn)
+    }
 }
