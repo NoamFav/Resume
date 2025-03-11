315bad5f78c6cf44921ece3617e055effd1c0505
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-03-11 15:17:16 +0100

    auto-commit

---
+++ migrations/V4_project_image.sql
@@ -0,0 +1,8 @@
+INSERT INTO project_image (project_id, image_id) VALUES
+    (1, ) --Resume
+    (2,) --Pot_Pot_Golf
+    (3, ) --QueryCrust
+    (4, )-- ShadowedHunter
+    (5,) -- Neovim
+    (6, ) -- Pynexus
+
